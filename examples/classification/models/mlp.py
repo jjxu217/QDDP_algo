124954a5588120885e2f017c99db7fc540d5b9ab
Jiajun Xu <jjxu217@gmail.com>
2020-09-15 15:04:34 -0700

    modify MLP

--- examples/classification/models/mlp.py
+++ examples/classification/models/mlp.py
@@ -9,17 +9,19 @@ __all__ = ['mlp']
 class MLP(nn.Module):
     def __init__(self, num_classes=10):
         super().__init__()
-        n_hid = 1000
+        n_hid = 20
         n_out = 10
         self.l1 = nn.Linear(28*28, n_hid)
         self.l2 = nn.Linear(n_hid, n_hid)
-        self.l3 = nn.Linear(n_hid, n_out)
+        self.l3 = nn.Linear(n_hid, n_hid)
+        self.l4 = nn.Linear(n_hid, n_out)
 
     def forward(self, x: torch.Tensor):
         x = x.view([-1, 28*28])
-        x = F.relu(self.l1(x))
-        x = F.relu(self.l2(x))
-        x = self.l3(x)
+        x = F.sigmoid(self.l1(x))
+        x = F.sigmoid(self.l2(x))
+        x = F.sigmoid(self.l3(x))
+        x = self.l4(x)
         return x
 
 
